798d45c3fc5788102f20c1d3d5a79383be8bea3d
Petra Daneva <petra.daneva95@gmail.com>
2018-03-23 23:48:31 +0200

    test

--- view/main.php
+++ view/main.php
@@ -1,4 +1,5 @@
 <main>
+    <h1>BLALLa</h1>
     <!-- Tova e samo za pylnej , che e mnogo golo i drazni:)) -->
     <img src="https://www.mmt.io/wp-content/uploads/2013/01/MMT-TransparentDisplays-HypeboxTouch47Dior-1170px-q80.jpg">
 </main>
